766399bcb8a5f4b28ab05331ae143f19014ebc37
tamii07 <32418021+tamii07@users.noreply.github.com>
2017-10-20 07:40:57 +0700

    Update line-tcr.py

--- line-tcr.py
+++ line-tcr.py
@@ -90,7 +90,7 @@ Setgroup =""" Privasi Menu V.1 􀔃􀄆red check mark􏿿
 -- Joinn on/off
 """
 KAC=[cl,ki,kk,kc,ks,ka,kb,ko,ke,ku]
-DEF=[ka,kb,ko,ke,ku]
+DEF=[ka,kb,ko,ke,ku,cl,ki,kk,kc,ks]
 mid = cl.getProfile().mid
 Amid = ki.getProfile().mid
 Bmid = kk.getProfile().mid
