8cb10ea4969483da1dcc1cbf146061496834d109
FewTeamTestBot <32820374+FewTeamBot@users.noreply.github.com>
2018-03-05 20:21:07 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/8.1.1"
-  LA = "DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0"
+  UA = "Line/1.4.17"
+  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
   rev = 0
 
