eb018a46be5d8db4638bf7d08700a53677e8adc0
FewTeamTestBot <32820374+FewTeamBot@users.noreply.github.com>
2018-03-05 17:42:03 +0700

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/6.0.0 iPad4,1 9.0.2"
-    LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
+    UA = "Line/8.1.1"
+    LA = "DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0"
 
     authToken = None
     mid = None
